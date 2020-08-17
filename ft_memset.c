5bbf805e90c6889d186ee1403295b233b95c0036
alexlazer1993 <58033778+alexlazer1993@users.noreply.github.com>
2020-08-17 17:10:23 +0200

    last update

--- ft_memset.c
+++ ft_memset.c
@@ -3,14 +3,11 @@
 void *ft_memset(void *b, int c, size_t len)
 {
     unsigned char *b2;
-    unsigned char c2;
 
-    c2 = (unsigned char)c;
-    b2 = (unsigned char *)b;
-    while (len--)
+    b2 = b;
+    while (len-- > 0)
     {
-        *b2 = c2;
-        b++;
+        *b2++ = (unsigned char)c;
     }
     return (b);
 }
