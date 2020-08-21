3d0ae39745c19b623e298df71f50fa1d3b7c6d12
alexlazer1993 <58033778+alexlazer1993@users.noreply.github.com>
2020-08-21 11:39:45 +0200

    ft_mapi

---
+++ ft_strmap.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char *str;
+    int i;
+
+    if (!s || !f)
+        return (NULL);
+    if (!(str = (char *)malloc((ft_strlen(s) + 1) * sizeof(char))))
+        return (0);
+    i = 0;
+    while (*s)
+    {
+        str[i] = (*f)(i, *s);
+        s++;
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
