57179bbf8f523d30f84f6931b645b5d01a49cc93
Alexandro Fulmini <alexandrofulmini@Alexandros-MBP.home>
2020-09-17 13:19:19 +0200

    correction

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afulmini <afulmini@student.42.fr>          +#+  +:+       +#+        */
+/*   By: afulmini <afulmini@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 14:58:30 by afulmini          #+#    #+#             */
 /*   Updated: 2020/08/23 16:20:45 by afulmini         ###   ########.fr       */
--- ft_substr.c
+++ ft_substr.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afulmini <afulmini@student.s19.fr>         +#+  +:+       +#+        */
+/*   By: afulmini <afulmini@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 14:58:30 by afulmini          #+#    #+#             */
 /*   Updated: 2020/09/17 16:20:45 by afulmini         ###   ########.fr       */
